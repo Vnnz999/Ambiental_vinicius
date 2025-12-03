8fd6c0ff5a6373b70ae2e1ce56516f34a1e9f016
Viniz <viniciuscaetano200050000@gmail.com>
2025-12-03 08:09:13 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request, redirect
+from flask_mail import Mail, Message
+
+app = Flask(__name__)
+
+# CONFIG DO EMAIL
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+
+# COLOQUE SEU E-MAIL E SENHA APP DO GMAIL
+app.config['MAIL_USERNAME'] = 'SEU_EMAIL@gmail.com'
+app.config['MAIL_PASSWORD'] = 'SUA_SENHA_DE_APP'
+
+mail = Mail(app)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/enviar_email', methods=['POST'])
+def enviar_email():
+    email_usuario = request.form['email']
+
+    msg = Message(
+        subject="Bem-vindo ao EcoBottle 🌱",
+        sender=app.config['MAIL_USERNAME'],
+        recipients=[email_usuario]
+    )
+
+    msg.body = """Obrigado por se inscrever na nossa newsletter!
+Aqui está seu guia exclusivo para um futuro sustentável 🌍
+
+Equipe EcoBottle 💚
+"""
+
+    mail.send(msg)
+
+    return redirect('/')  # depois podemos voltar com modal via JS
+
+if __name__ == '__main__':
+    app.run(debug=True)
